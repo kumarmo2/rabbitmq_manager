1b91c34778b45f709ce48ca94fe471dff2762a92
kumarmo2 <kr.mohit6794@gmail.com>
2020-04-27 23:48:02 +0530

    made somewhat stable library which needs urgent performance boost

--- manager/src/lib.rs
+++ manager/src/lib.rs
@@ -1,26 +1,89 @@
 #![allow(warnings)]
 
 use lapin::{
-    Channel, ChannelStatus, CloseOnDrop, Connection, ConnectionProperties, ConnectionState,
+    options::BasicPublishOptions, BasicProperties, Channel, ChannelStatus, CloseOnDrop, Connection,
+    ConnectionProperties, ConnectionState,
 };
 use r2d2::{CustomizeConnection, ManageConnection, Pool};
+use serde::{Deserialize, Serialize};
+use serde_json::to_vec;
 use std::sync::{Arc, Mutex};
 use tokio::runtime::{Builder, Runtime};
 
-struct RabbitMqManager {
+pub struct RabbitMqManager {
     channel_pool: Pool<ChannelManager>,
+    //After initial somewhat stabilization of the library, remove tokio, and use a light weight library.
     runtime: Arc<Mutex<Runtime>>,
 }
 
-struct ConnectionManager {
-    runtime: &'static mut Runtime,
-    uri: &'static str,
+impl RabbitMqManager {
+    pub fn new(uri: &'static str) -> Self {
+        let mut runtime = Builder::new()
+            .threaded_scheduler()
+            .build()
+            .expect("could not create runtime");
+
+        let guarded_runtime = Arc::new(Mutex::new(runtime));
+        let connection_manager = ConnectionManager::new(uri, Arc::clone(&guarded_runtime));
+        let channel_manager = ChannelManager::new(connection_manager, Arc::clone(&guarded_runtime));
+        let channel_pool = Pool::builder()
+            .max_size(18)
+            .build(channel_manager)
+            .expect("could not create channel pool");
+
+        RabbitMqManager {
+            channel_pool,
+            runtime: guarded_runtime,
+        }
+    }
 }
 
+// Make sure queue exists before calling this method.
+impl RabbitMqManager {
+    pub fn publish_message_to_queue<T: Serialize>(
+        &self,
+        queue_name: &str,
+        payload: &T,
+    ) -> Result<(), lapin::Error> {
+        let payload_vec = to_vec(payload).unwrap();
+        let result = Arc::clone(&self.runtime)
+            .lock()
+            .unwrap()
+            .block_on(async move {
+                return self
+                    .channel_pool
+                    .clone()
+                    .get()
+                    .unwrap()
+                    .basic_publish(
+                        "",
+                        "hello",
+                        BasicPublishOptions::default(),
+                        payload_vec,
+                        BasicProperties::default(),
+                    )
+                    .await;
+            });
+        match result {
+            Ok(_) => {
+                return Ok(());
+            }
+            Err(reason) => {
+                // TODO: this looks awkard. Should be a better syntax.
+                return Err(reason);
+            }
+        }
+    }
+}
+
+struct ConnectionManager {
+    runtime: Arc<Mutex<Runtime>>,
+    uri: &'static str,
+}
 impl ConnectionManager {
-    fn new(connection_string: &'static str, runtime: &'static mut Runtime) -> Self {
+    fn new(connection_string: &'static str, runtime: Arc<Mutex<Runtime>>) -> Self {
         ConnectionManager {
-            runtime,
+            runtime: runtime,
             uri: connection_string,
         }
     }
@@ -30,15 +93,34 @@ struct ChannelManager {
     // TODO: need to think how can I split channels among different connections after certain limit.
     // current_num_of_channels: Arc<Mutex<i32>>,
     connection_pool: Pool<ConnectionManager>,
-    runtime: &'static mut Runtime,
+    // Need to knwo if we can use a RefCell for this?
+    runtime: Arc<Mutex<Runtime>>,
+}
+
+impl ChannelManager {
+    fn new(connection_manager: ConnectionManager, rt: Arc<Mutex<Runtime>>) -> Self {
+        let connection_pool = Pool::builder()
+            // TODO: need to make sure a new connection should only be made, when the existing connections
+            // have reached the channels per connection quota.
+            // it is very much needed for performance reasons.
+            .max_size(100) // TODO: make this size configurable.
+            .build(connection_manager)
+            .expect("could not create connection pool");
+
+        ChannelManager {
+            connection_pool,
+            runtime: rt,
+        }
+    }
 }
 
 impl ManageConnection for ChannelManager {
     type Error = lapin::Error;
     type Connection = CloseOnDrop<Channel>;
     fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        return self
-            .runtime
+        return Arc::clone(&self.runtime)
+            .lock()
+            .unwrap()
             .block_on(self.connection_pool.clone().get().unwrap().create_channel());
     }
     fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
@@ -60,9 +142,12 @@ impl ManageConnection for ConnectionManager {
     type Connection = CloseOnDrop<lapin::Connection>;
 
     fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        return self.runtime.block_on(async move {
-            return lapin::Connection::connect(self.uri, ConnectionProperties::default()).await;
-        });
+        return Arc::clone(&self.runtime)
+            .lock()
+            .unwrap()
+            .block_on(async move {
+                return lapin::Connection::connect(self.uri, ConnectionProperties::default()).await;
+            });
     }
 
     fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
---
+++ manager_lib_tester/src/main.rs
@@ -0,0 +1,28 @@
+use manager::RabbitMqManager;
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+
+//TODO: in the library, remove most of the calls to unwrap.
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyStruct {
+    name: String,
+}
+
+fn main() {
+    let addr = "amqp://guest:guest@127.0.0.1:5672/%2f";
+    let x = MyStruct {
+        name: "kumarmo2".to_string(),
+    };
+    let manager = RabbitMqManager::new(addr);
+    for count in 0..100 {
+        match manager.publish_message_to_queue("hello", &x) {
+            Ok(_) => println!("published {} message", count),
+            Err(reason) => {
+                println!("could not publish the message: {}", reason);
+            }
+        }
+    }
+    std::thread::sleep(Duration::from_secs(100));
+    println!("Hello, world!");
+}
