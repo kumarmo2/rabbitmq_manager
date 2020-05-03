b6364d9faefd8771600541f2a0f05f23a3fc120d
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-04 02:33:35 +0530

    modified main function to load the message_service.

--- manager_lib_tester/src/main.rs
+++ manager_lib_tester/src/main.rs
@@ -1,5 +1,7 @@
+use chat_common_types::events::{MessageEvent, MessageEventType};
 use manager::RabbitMqManager;
 use serde::{Deserialize, Serialize};
+use smol;
 use std::time::Duration;
 
 //TODO: in the library, remove most of the calls to unwrap.
@@ -15,14 +17,28 @@ fn main() {
         name: "kumarmo2".to_string(),
     };
     let manager = RabbitMqManager::new(addr);
-    // for count in 0..100 {
-    match manager.publish_message_to_queue_sync("hello", &x) {
-        // Ok(_) => println!("published {} message", count),
-        Ok(_) => println!("published message"),
-        Err(reason) => {
-            println!("could not publish the message: {}", reason);
-        }
+    let event = MessageEvent {
+        id: 324,
+        user_id: 12,
+        event_type: MessageEventType::Send,
+    };
+    let cpus = num_cpus::get().max(1);
+    for _ in 0..cpus {
+        std::thread::spawn(move || smol::run(futures::future::pending::<()>()));
     }
-    // }
-    // std::thread::sleep(Duration::from_secs(100));
+
+    smol::run(async move {
+        loop {
+            match manager
+                .publish_message_to_queue_async("messages", &event)
+                .await
+            {
+                // Ok(_) => println!("published {} message", count),
+                Ok(_) => println!("published message"),
+                Err(reason) => {
+                    println!("could not publish the message: {}", reason);
+                }
+            }
+        }
+    });
 }
